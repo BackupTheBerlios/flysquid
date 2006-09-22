50a34d4c63caf37dbc222a8f6ffb63a1267f7afd
ahiliation <ahiliation>
2006-09-22 14:40:13 +0000

    update

--- Flysquid/src/main.c
+++ Flysquid/src/main.c
@@ -18,11 +18,10 @@
 
 #include<gtk/gtk.h>
 #include<gdk/gdk.h>
-
 GtkWidget *FSstartwindow;
 startserver   ()
 {
-	
+        netfly(); 	
         FSstartwindow = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_type_hint(GTK_WINDOW(FSstartwindow),GDK_WINDOW_TYPE_HINT_MENU);
 	gtk_window_set_resizable(GTK_WINDOW(FSstartwindow),FALSE);
